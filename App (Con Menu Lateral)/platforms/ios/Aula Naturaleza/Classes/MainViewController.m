db6a0c0477fba39aeaef4c56ba5314ffb46d0448
Ulises <uliseslevi@gmail.com>
2017-10-03 17:41:24 +0100

    compilacion full

--- App (Con Menu Lateral)/platforms/ios/Aula Naturaleza/Classes/MainViewController.m
+++ App (Con Menu Lateral)/platforms/ios/Aula Naturaleza/Classes/MainViewController.m
@@ -19,7 +19,7 @@
 
 //
 //  MainViewController.h
-//  hijaCambada
+//  Aula Naturaleza
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
